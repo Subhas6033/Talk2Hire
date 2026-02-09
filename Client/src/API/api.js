9805ddbd7476fcc4dcfa6db1fb59522e05899c21
Subhas <goalkeepersubhas07@gmail.com>
2026-02-09 12:14:14 +0530

    fix

--- Client/src/API/api.js
+++ Client/src/API/api.js
@@ -3,9 +3,9 @@ import axios from "axios";
 const api = axios.create({
   baseURL: `${import.meta.env.VITE_BACKEND_URL}/api/v1`,
   withCredentials: true,
-  headers: {
+  /* headers: {
     "Content-Type": "application/json",
-  },
+  }, */
 });
 
 //  Log all API requests for debugging
